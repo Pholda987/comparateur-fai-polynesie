df73006900271ce59f3692432a24f302dd2165b0
Heritage <teturu.adolph@gmail.com>
2026-04-05 18:06:15 -1000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
     vodafone: {
       name: 'Vodafone',
       logo: 'https://www.vodafone.pf/images/logo.png',
-      color: '#F28705',
+      color: '#8C1F28',
       tagline: "La puissance d'un réseau mondial",
       description: 'Entreprise 100% polynésienne avec expertise internationale',
       coverage: 3, speed: 4, price: 4, support: 5,
